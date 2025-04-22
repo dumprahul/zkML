93b1fffa2678153da122d2827a7bdc3a95b7f846
dumprahul <shanrahul04@gmail.com>
2025-04-22 06:30:09 +0530

    ui-commit-landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -149,14 +149,14 @@ export default function LandingPage() {
                 className="block dark:hidden"
                 animationStyle="top-in-bottom-out"
                 videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-                thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+                thumbnailSrc="/ss.png"
                 thumbnailAlt="Hero Video"
               />
               <HeroVideoDialog
                 className="hidden dark:block"
                 animationStyle="top-in-bottom-out"
                 videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-                thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+                thumbnailSrc="/ss.png"
                 thumbnailAlt="Hero Video"
               />
             </div>
