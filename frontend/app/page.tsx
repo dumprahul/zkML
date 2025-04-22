b87e6bee3af2710152fc4b45c1aed97937186249
dumprahul <shanrahul04@gmail.com>
2025-04-22 10:30:17 +0530

    frontend-ui-change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -148,14 +148,14 @@ export default function LandingPage() {
               <HeroVideoDialog
                 className="block dark:hidden"
                 animationStyle="top-in-bottom-out"
-                videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+                videoSrc="https://www.youtube.com/embed/vV8HDszWtwE"
                 thumbnailSrc="/ss.png"
                 thumbnailAlt="Hero Video"
               />
               <HeroVideoDialog
                 className="hidden dark:block"
                 animationStyle="top-in-bottom-out"
-                videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+                videoSrc="https://www.youtube.com/embed/vV8HDszWtwE"
                 thumbnailSrc="/ss.png"
                 thumbnailAlt="Hero Video"
               />
